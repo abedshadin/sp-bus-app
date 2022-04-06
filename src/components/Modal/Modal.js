04dc7849f229fe9efaeb023b0b9f655968f2cc98
abedshadin <abedshadin@gmail.com>
2022-04-06 22:40:27 +0600

    modal footer remove

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -124,18 +124,7 @@ Image Credits : {b_img_out_credit}
 </table>
 </div>
                 </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                 
-                </div>
-              </div>
+               </div>
             </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
